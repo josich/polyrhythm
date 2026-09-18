c4a502c145fb5adc01aba7939c1e4f5fc01369eb
Oscar <josich@users.noreply.github.com>
2026-09-18 15:05:27 +0200

    Polyorbit v208

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Offline cache for Polyorbit.
    Bump CACHE whenever you redeploy, or phones will keep serving the old app. */
-var CACHE = "polyrhythm-v207";
+var CACHE = "polyrhythm-v208";
 /* proof that a response really is this app and not a host's error page */
 var MARK = "polyrhythm-circle:session";
 var ASSETS = [
